84334628ae9fd8b9855d677860f133bb2355c6ca
nil1729 <nilanjan172nsvian@gmail.com>
2024-02-27 20:41:02 +0000

    add cloudflare reverse proxy (tunnel)

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,1 +1,5 @@
 SQLALCHEMY_DATABASE_URI = 'postgresql://superset:password@superser-backend-db:5432/superset'
+
+# load balancer
+# https://superset.apache.org/docs/installation/configuring-superset/#configuration-behind-a-load-balancer
+ENABLE_PROXY_FIX = True
